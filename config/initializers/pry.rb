50c68c0235e044b68b874bf5674adddf9534c761
Melidios One <melidiosone@gmail.com>
2020-11-30 16:43:50 -0500

    Pry fix 2

--- config/initializers/pry.rb
+++ config/initializers/pry.rb
@@ -1,10 +1,1 @@
-original_prompt = Pry.config.prompt
-
-if Rails.env.development? || Rails.env.test?
-  color = :green
-  env   = Rails.env.upcase
-end
-
-debug = (Rails.env == 'debug' ? ' (DEBUG)' : '')
-
-Pry.config.prompt_name = Pry::Helpers::Text.send(color, env + debug)
+Pry.config.prompt
