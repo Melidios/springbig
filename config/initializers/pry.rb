4de08442b89790ef45d4344d91aa8a1c9dc91c70
Melidios One <melidiosone@gmail.com>
2020-11-30 16:37:57 -0500

    Pry fix

--- config/initializers/pry.rb
+++ config/initializers/pry.rb
@@ -1,12 +1,8 @@
 original_prompt = Pry.config.prompt
 
-# url = ENV['CANONICAL_URL']
 if Rails.env.development? || Rails.env.test?
   color = :green
   env   = Rails.env.upcase
-else
-  color = :magenta
-  env   = url.match(/https:\/\/((\w|-)+)/).try(:[], 1)&.upcase || ''
 end
 
 debug = (Rails.env == 'debug' ? ' (DEBUG)' : '')
